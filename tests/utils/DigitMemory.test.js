31e9a2ca7f0e4a77d67439b4794178e592522baf
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-05-09 08:03:17 +0330

    in the middle of input validation test implementation...

--- tests/utils/DigitMemory.test.js
+++ tests/utils/DigitMemory.test.js
@@ -7,20 +7,20 @@ describe("DigitMemory", () => {
       const digitMemory = new Warmup();
       digitMemory.input = { number: '13 u 8 a *', digits: 317 };
 
-      const Front = `4 5 <span style="color: rgb(170, 255, 0);">*</span> <span style="color: rgb(170, 255, 0);">*</span> 0 <span style="color: rgb(170, 255, 0);">*</span> <span style="color: rgb(170, 255, 0);">*</span>`;
+      // const Front = `4 5 <span style="color: rgb(170, 255, 0);">*</span> <span style="color: rgb(170, 255, 0);">*</span> 0 <span style="color: rgb(170, 255, 0);">*</span> <span style="color: rgb(170, 255, 0);">*</span>`;
 
-      const Answer = "3 1 7 1";
+      // const Answer = "3 1 7 1";
 
-      const TTSFront = "4 5 3 1 0 7 1";
+      // const TTSFront = "4 5 3 1 0 7 1";
 
-      const TTSBack = "4 5 3 1 0 7 1";
+      // const TTSBack = "4 5 3 1 0 7 1";
 
-      const Tags = "Memo DigitMemory Warmup Forward 7Digits";
+      // const Tags = "Memo DigitMemory Warmup Forward 7Digits";
 
-      const correctOutput = [Front, Answer, TTSFront, TTSBack, Tags].join`|`;
+      // const correctOutput = [Front, Answer, TTSFront, TTSBack, Tags].join`|`;
       
-      const result = digitMemory.output;
-      expect(result).toBe(correctOutput);
+      // const result = digitMemory.output;
+      // expect(result).toBe(correctOutput);
     });
   });
   it(`const digitMemory = new DigitMemory();\n\tdigitMemory.number = 4531071;\n\t digitMemory.input = { number: 4531071, digits: 317 };`, () => {
