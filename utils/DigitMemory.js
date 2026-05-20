5a29a7ed509079401af577466e00bd788880d19b
Morteza Bakhshandeh <bcfcode@gmail.com>
2026-05-20 15:53:39 +0330

    digit number bug fixed

--- utils/DigitMemory.js
+++ utils/DigitMemory.js
@@ -12,7 +12,7 @@ export class Warmup {
 
   #addSpace = (n) => [...`${n}`].join` `;
 
-  #removeSpace = (n) => `${n}`.replace(/ /g, '')
+  #removeSpace = (n) => `${n}`.replace(/ /g, "");
 
   #getReg = (n) => {
     return new RegExp(`[${n}]`, "g");
@@ -21,12 +21,12 @@ export class Warmup {
   #createColoredAsterisk = (color) => {
     return `<span style="color: ${color}">*</span>`;
   };
- 
+
   #canNotConvertToNumber = (number) => {
     if (!/number|string/.test(typeof number)) return true;
     if (typeof number === "string") {
       if (number === "") return true;
-      const cleanNumber = this.#removeSpace(number)
+      const cleanNumber = this.#removeSpace(number);
       const hasNoneDigit = /\D/.test(cleanNumber);
       if (hasNoneDigit) return true;
     }
@@ -34,7 +34,8 @@ export class Warmup {
   };
 
   #isValidInputs = () => {
-   if( [this.#number, this.#digits].some(this.#canNotConvertToNumber)) return false;
+    if ([this.#number, this.#digits].some(this.#canNotConvertToNumber))
+      return false;
 
     const reg = this.#getReg(this.#digits);
     const numberHasDigits = reg.test(`${this.#number}`);
@@ -73,7 +74,7 @@ export class Warmup {
   };
 
   #createNumberOfDigitsTag = () => {
-    const numberOf = this.#number.toString().length;
+    const numberOf = this.#removeSpace(this.#number).toString().length;
     return ` ${numberOf}Digits`;
   };
 
@@ -137,7 +138,7 @@ export class Warmup {
   get output() {
     return this.#createOutput();
   }
- 
+
   outputToFile = (path = "Anki.txt") =>
     fs.writeFileSync(`outputs/${path}`, this.#createOutput(), "utf8");
 }
@@ -152,6 +153,39 @@ const inputs = [
   ["7 1 3 4 8 6 0 6 2 6", 87],
   ["4 0 9 7 9 4 0 5 1", 74],
   ["3 2 3 8 2 9 5 3 6 2", 82519],
+  ["7 1 3 4 8 6 0 6 2 6", 54],
+  ["4 0 9 7 9 4 0 5 1", 4],
+  ["6 8 0 6 7 1 5 6", 7],
+  ["4 0 9 7 9 4 0 5 1", 47],
+  ["7 6 1 0 7 8 6 9 0", 71],
+  ["4 0 9 7 9 4 0 5 1", 74],
+  ["8 3 1 4 5 3 1 2 7", 5314],
+  ["0 8 6 3 6 1 3 7 9", 67],
+  ["4 7 1 9 0 8 3 7 8", 837],
+  ["8 9 0 5 8 6 4 3 6", 40],
+  ["8 3 1 4 5 3 1 2 7", 8143],
+  ["8 9 0 5 8 6 4 3 6", 95],
+  ["3 8 6 9 2 4 9 7 0", 467],
+  ["8 5 2 0 2 1 4 3 7 9", 4],
+  ["9 4 6 5 1 8 5 1 9", 45],
+  ["8 3 1 4 5 3 1 2 7", 5],
+  ["6 4 3 9 1 7 1 3 6 5", 1],
+  ["7 5 1 6 4 3 9 0 4", 16370],
+  ["8 5 2 0 2 1 4 3 7 9", 4],
+  ["8 3 1 4 5 3 1 2 7", 462],
+  ["8 5 6 5 3 9 7 0", 973],
+  ["8 2 1 3 5 6 3 7 5", 67],
+  ["5 9 3 9 8 5 1 6 9", 853],
+  ["8 3 1 4 5 3 1 2 7", 4351],
+  ["8 5 6 5 3 9 7 0", 973],
+  ["1 9 2 8 6 4 5 3", 46],
+  ["8 2 1 3 5 6 3 7 5", 6],
+  ["8 9 7 1 6 0 3 0 6", 79630],
+  ["1 3 6 8 9 6 8 5 3 9", 91],
+  ["8 9 7 1 6 0 3 0 6", 91],
+  ["8 9 7 1 6 0 3 0 6", 790],
+  ["1 3 6 8 9 6 8 5 3 9", 59],
+  ["9 4 5 0 7 1 0 2 8 6", 102],
 ];
 
 inputs.forEach(([number, digits, isBackward]) => {
